e5ba1b3385668535ef4954a139daf8d1d4963e34
Сергей Лукьянчук <lukianchuk.s@gmail.com>
2021-04-02 00:10:09 +0300

    JS_HW2

---
+++ js/HW_2/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const userNum = prompt("Enter the number:");
+if (
+  !isNaN(userNum) &&
+  userNum !== null &&
+  userNum.trim() !== "" &&
+  +userNum >= 0
+) {
+  for (let i = 0; i <= +userNum; i++) {
+    if (i % 5 === 0) {
+      console.log(i);
+    }
+  }
+} else {
+  console.log("Sorry, no numbers");
+}
