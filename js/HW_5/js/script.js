43f220439a346c8781baa69b0a60770176ccb1c7
Сергей Лукьянчук <lukianchuk.s@gmail.com>
2021-05-03 15:18:27 +0300

    HW5 redone

--- js/HW_5/js/script.js
+++ js/HW_5/js/script.js
@@ -1,44 +1,38 @@
 `use strict`;
-const name = prompt("Введите имя");
-const surname = prompt("Введите фамилию");
-const birth = prompt("Введите дату рождения в формате dd.mm.yyyy");
-function createNewUser(firstName, lastName, birthday) {
-  const newUser = {
-    firstName,
-    lastName,
-    getLogin() {
-      return (this.firstName[0] + this.lastName).toLowerCase();
-    },
-    birthday,
-    getAge() {
-      year = this.birthday.substring(6, 10);
-      mounth = this.birthday.substring(3, 5);
-      day = this.birthday.substring(0, 2);
-      let now = new Date();
-      let dob = new Date(year, mounth, day);
-      age = now.getFullYear() - dob.getFullYear();
-      if (
-        now.getMonth() < dob.getMonth() ||
-        (now.getMonth() == dob.getMonth() && now.getDate() < dob.getDate())
-      ) {
-        age = age - 1;
-      }
-      return age;
-    },
-    getPassword() {
-      return (
-        this.firstName[0].toUpperCase() +
-        this.lastName.toLowerCase() +
-        this.birthday.substring(6, 10)
-      );
-    },
+
+function createNewUser() {
+  this.firstName = prompt("Введите имя");
+  this.lastName = prompt("Введите фамилию");
+  this.birthday = prompt("Введите дату рождения в формате dd.mm.yyyy");
+  this.getLogin = function () {
+    let login = (this.firstName[0] + this.lastName).toLowerCase();
+    return login;
   };
-  newUser.age = newUser.getAge();
-  newUser.login = newUser.getLogin();
-  newUser.password = newUser.getPassword();
-  console.log(newUser.age);
-  console.log(newUser.password);
-  return newUser;
+  this.getAge = function () {
+    let year = this.birthday.substring(6, 10);
+    let mounth = this.birthday.substring(3, 5);
+    let day = this.birthday.substring(0, 2);
+    let now = new Date();
+    let dob = new Date(year, mounth, day);
+    let age = now.getFullYear() - dob.getFullYear();
+    if (
+      now.getMonth() < dob.getMonth() ||
+      (now.getMonth() == dob.getMonth() && now.getDate() < dob.getDate())
+    ) {
+      age = age - 1;
+    }
+    return age;
+  };
+  this.getPassword = function () {
+    return (
+      this.firstName[0].toUpperCase() +
+      this.lastName.toLowerCase() +
+      this.birthday.substring(6, 10)
+    );
+  };
 }
 
-console.log(createNewUser(name, surname, birth));
+let newUser = new createNewUser();
+console.log(newUser.getLogin());
+console.log(newUser.getAge());
+console.log(newUser.getPassword());
