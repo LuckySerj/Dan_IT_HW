1b08de5134ab9f7b3115b42071d564bb4608365f
Сергей Лукьянчук <lukianchuk.s@gmail.com>
2021-05-23 18:09:18 +0300

    HW9_JS

---
+++ js/HW_9/js/script.js
@@ -0,0 +1,27 @@
+`use strict`;
+
+const tab = function () {
+  const tabNav = document.querySelectorAll(`.tabs-title`);
+  const tabContent = document.querySelectorAll(`.tab-content`);
+
+  tabNav.forEach((item) => {
+    item.addEventListener(`click`, selectTabNav);
+  });
+
+  function selectTabNav() {
+    tabNav.forEach((item) => item.classList.remove(`active`));
+    this.classList.add(`active`);
+    let tabName = this.getAttribute(`data-tab-name`);
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    tabContent.forEach((item) => {
+      item.classList.contains(tabName)
+        ? item.classList.add(`active`)
+        : item.classList.remove(`active`);
+    });
+  }
+};
+
+tab();
