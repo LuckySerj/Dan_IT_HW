4c7cbce3f619daba224e2e4f3f040c3784ba47e7
Сергей Лукьянчук <lukianchuk.s@gmail.com>
2021-05-23 15:07:26 +0300

    HW8_JS

--- js/HW_8/js/script.js
+++ js/HW_8/js/script.js
@@ -1,21 +1,50 @@
 `use strict`;
 
-let input = document.createElement(`input`);
-input.type = `text`;
+const input = document.createElement(`input`);
+input.type = `number`;
+input.style.outline = `none`;
+input.style.borderRadius = `2px`;
 document.body.append(input);
-let span = document.createElement(`span`);
+
+const inputDesc = document.createElement(`span`);
+inputDesc.innerHTML = `<br>Price, $  `;
+input.before(inputDesc);
+
+const span = document.createElement(`span`);
+
+span.style.padding = `1px`;
+console.log(input.value);
+
+const button = document.createElement(`button`);
+button.innerHTML = `X`;
+button.style.fontSize = `50%`;
+
+const wrongInput = document.createElement(`p`);
+wrongInput.innerHTML = `Please enter correct price`;
 
 input.addEventListener(`focus`, focus);
 function focus() {
-  input.style.outlineColor = `green`;
+  input.style.border = `2px solid green`;
+  wrongInput.remove();
 }
-// input.addEventListener(`blur`, spanPrice);
+
 input.onblur = function () {
-  let price = +input.value;
+  let price = input.value;
+  span.innerHTML = `Your price: ${price}`;
+  input.style.border = `2px solid black`;
+  input.style.color = `black`;
   if (price > 0) {
-    span.innerHTML = `Текущая цена: ${input.value}`;
-    input.after(span);
-  } else {
-    console.log(`что-то не так`);
+    inputDesc.before(span);
+    input.style.color = `green`;
+    span.append(button);
+    button.onclick = function () {
+      button.parentElement.remove();
+      input.value = ``;
+      return;
+    };
+  } else if (price < 0) {
+    input.style.border = `2px solid red`;
+    input.style.color = `black`;
+    input.after(wrongInput);
   }
 };
